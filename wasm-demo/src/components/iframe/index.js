b2335c95bf95f1e0bc8abf64ddc09ce5ba5144ff
dufresnesteven <steve.dufresne@automattic.com>
2022-11-30 13:56:10 +0900

    Move block into the theme.

--- wasm-demo/src/components/iframe/index.js
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from '@wordpress/element';
-
-export default forwardRef( ( { src }, ref ) => {
-	return (
-		<iframe title="WordPress.org WASM sandbox" src={ src } ref={ ref } />
-	);
-} );
